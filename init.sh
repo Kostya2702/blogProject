d69a4af934412214e5017435c985c25145da2495
Kostya2702 <loshkarev.kostya27@gmail.com>
2021-09-10 17:55:00 +0500

    qa/urls.py

--- init.sh
+++ init.sh
@@ -1,4 +1,4 @@
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
-sudo ln -sf /home/box/web/ask/django.py /etc/gunicorn.d/django.py
-sudo gunicorn -c /etc/gunicorn.d/django.py ask.django:wsgi_app
+sudo ln -sf /home/box/web/etc/qa_conf.py /etc/gunicorn.d/qa_conf.py
+sudo gunicorn -c /etc/gunicorn.d/qa_conf.py ask.django:wsgi_app
