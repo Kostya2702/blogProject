d6ca9b5581d53fa8c1bcbb4aa2c2d809b41c33a7
Kostya2702 <loshkarev.kostya27@gmail.com>
2021-09-10 17:29:40 +0500

    rename conf

--- ask/qa/urls.py
+++ ask/qa/urls.py
@@ -2,11 +2,11 @@ from django.conf.urls import url
 from qa.views import test
 
 urlpatterns = [
-    url('', test),
-    url('login/', test, name='login'),
-    url('signup/', test, name='signup'),
-    url('question/<27>/', test, name='question'),
-    url('ask/', test, name='ask'),
-    url('popular/', test, name='popular'),
-    url('new/', test, name='new'),
+    url('^$', test, name='index'),
+    url('^login/.*$', test, name='login'),
+    url('^signup/.*$', test, name='signup'),
+    url('^question/<27>/.*$', test, name='question'),
+    url('^ask/.*$', test, name='ask'),
+    url('^popular/.*$', test, name='popular'),
+    url('^new/.*$', test, name='new'),
 ]
