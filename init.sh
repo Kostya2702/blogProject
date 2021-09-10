b85531d713b69dd5e5f9a078043e22a8db5b0c59
Kostya2702 <loshkarev.kostya27@gmail.com>
2021-09-10 17:39:32 +0500

    refactor conf

--- init.sh
+++ init.sh
@@ -1,4 +1,4 @@
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
-sudo ln -sf /home/box/web/django.py /etc/gunicorn.d/django.py
+sudo ln -sf /home/box/web/ask/django.py /etc/gunicorn.d/django.py
 sudo gunicorn -c /etc/gunicorn.d/django.py ask.django:wsgi_app
