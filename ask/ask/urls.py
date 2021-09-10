297ed223da018ef73b76152cfc71105ac8bd7a72
Kostya2702 <loshkarev.kostya27@gmail.com>
2021-09-10 18:04:30 +0500

    urls.py

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -3,12 +3,12 @@ from django import admin
 from django import include
 
 urlpatterns = [
-    url('^admin/.*$', admin.site.urls),
+    url('^admin/', admin.site.urls),
     url('^$', include('qa.urls')),
-    url('^login/.*$', include('qa.urls')),
-    url('^signup/.*$', include('qa.urls')),
-    url('^question/<27>/.*$', include('qa.urls')),
-    url('^ask/.*$', include('qa.urls')),
-    url('^popular/.*$', include('qa.urls')),
-    url('^new/.*$', include('qa.urls')),
+    url('^login/$', include('qa.urls')),
+    url('^signup/$', include('qa.urls')),
+    url('^question/\d+/$', include('qa.urls')),
+    url('^ask/$', include('qa.urls')),
+    url('^popular/.', include('qa.urls')),
+    url('^new/$', include('qa.urls')),
 ]
--- ask/qa/urls.py
+++ ask/qa/urls.py
@@ -3,10 +3,10 @@ from qa.views import test
 
 urlpatterns = [
     url('^$', test, name='index'),
-    url('^login/.*$', test, name='login'),
-    url('^signup/.*$', test, name='signup'),
-    url('^question/<27>/.*$', test, name='question'),
-    url('^ask/.*$', test, name='ask'),
-    url('^popular/.*$', test, name='popular'),
-    url('^new/.*$', test, name='new'),
+    url('^login/$', test, name='login'),
+    url('^signup/$', test, name='signup'),
+    url('^question/\d+/$', test, name='question'),
+    url('^ask/$', test, name='ask'),
+    url('^popular/$', test, name='popular'),
+    url('^new/$', test, name='new'),
 ]
