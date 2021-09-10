193d319ea9aa862941a5b2637e92e05d0738ed2f
Kostya2702 <loshkarev.kostya27@gmail.com>
2021-09-10 17:20:10 +0500

    init.sh

--- init.sh
+++ init.sh
@@ -1,4 +1,4 @@
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
-sudo ln -sf /home/box/web/django.py /etc/gunicorn.d/ask_conf.py
+sudo ln -sf /home/box/web/django.py /etc/gunicorn.d/django.py
 sudo gunicorn -c /etc/gunicorn.d/django.py django:wsgi_app
