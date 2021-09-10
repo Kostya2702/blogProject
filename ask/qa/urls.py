9ebe766b6cbebdb24acc1fe1b4f9d3f9df2199c4
Kostya2702 <loshkarev.kostya27@gmail.com>
2021-09-10 17:15:29 +0500

    Big patch 0.1

--- ask/qa/urls.py
+++ ask/qa/urls.py
@@ -2,5 +2,11 @@ from django.conf.urls import url
 from qa.views import test
 
 urlpatterns = [
-    url('.*?', test)
+    url('', test),
+    url('login/', test, name='login'),
+    url('signup/', test, name='signup'),
+    url('question/<27>/', test, name='question'),
+    url('ask/', test, name='ask'),
+    url('popular/', test, name='popular'),
+    url('new/', test, name='new'),
 ]
