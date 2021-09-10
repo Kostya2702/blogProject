7e2cc9f301aac7075c01860ddf4b248e4510f7b2
Kostya2702 <loshkarev.kostya27@gmail.com>
2021-09-10 16:55:32 +0500

    Ficksed init.sh

--- init.sh
+++ init.sh
@@ -1,4 +1,4 @@
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
-gunicorn -c /home/box/web/etc/hello.py hello:wsgi_app
-gunicorn -c /home/box/web/etc/django.py django:wsgi_pp
+gunicorn -c /home/box/web/hello.py hello:wsgi_app
+gunicorn -c /home/box/web/django.py django:wsgi_pp
