1cb5a1eb81c74bbb2af201f1b8dc636a45fdc7bd
Kostya2702 <loshkarev.kostya27@gmail.com>
2021-09-12 18:34:11 +0500

    create models without migrations

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -1,1 +1,32 @@
-# Create your models here.
+from django.contrib.auth.models import User
+from django.db import models
+
+
+class QuestionManager(models.Manager):
+    def new(self):
+        return super().get_queryset().order_by('-added_at')
+
+    def popular(self):
+        return super().get_queryset().order_by('-likes')
+
+
+class Question(models.Model):
+    title = models.CharField(max_length=100)
+    text = models.TextField()
+    added_at = models.DateTimeField(auto_now=True)
+    rating = models.IntegerField(default=0)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    likes = models.ManyToManyField(User, on_delete=models.CASCADE)
+
+    objects = models.Manager()
+    question_text = QuestionManager()
+
+    def __unicode__(self):
+        return self.title
+
+
+class Answer(models.Model):
+    text = models.TextField()
+    added_at = models.DateField(auto_now=True)
+    question = models.ManyToManyField(User, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
