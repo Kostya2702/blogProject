b4456f90f64085f5905588021b36bac7135f86a3
Kostya2702 <loshkarev.kostya27@gmail.com>
2021-09-14 16:11:38 +0500

    change model

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -29,8 +29,9 @@ class Question(models.Model):
 class Answer(models.Model):
     text = models.TextField()
     added_at = models.DateField(auto_now=True)
-    question = models.ManyToManyField(User,
-                                      related_name='likes_answer')
+    question = models.ForeignKey(User,
+                                 on_delete=models.CASCADE,
+                                 related_name='text_question')
     author = models.ForeignKey(User,
                                on_delete=models.CASCADE,
                                related_name='author_answer')
